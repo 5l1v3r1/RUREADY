7854c1ac45aa046dfeeed73b78e806b6b0340fff
Wh0rU <60551533+Uayeb5@users.noreply.github.com>
2020-08-11 20:26:51 +0000

    Update ruready.py

--- ruready.py
+++ ruready.py
@@ -13,8 +13,8 @@ from colorama import Fore, Style
 
 ascii_banner = pyfiglet.figlet_format("R U READY ?")
 print(ascii_banner)
-print("RUREADY V2.0 - A TOOL BY UAYEB5")
-print("WARNING ! THIS TOOL IS FOR EDUCATIONAL PURPOSE ONLY !\n")
+print("RUREADY - A TOOL BY UAYEB5")
+print("This tool is for educational purpose only !\n")
 print(" ")
 
 time.sleep(2.0)
@@ -31,92 +31,95 @@ except:
 	print(Style.RESET_ALL)
 
 if b == '1':
-    print(" ")
 
-    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    def sql(): 
 
-    MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+    	USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
-    fin = open('sqdo.txt')
-
-    for line in fin:
+    	MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
     
-        query = line
-        query = query.replace(' ', '+')
-        URL = f"https://google.com/search?q={query}"
+    	fin = open('sqdo.txt')
 
-        headers = {"user-agent": USER_AGENT}
-        resp = requests.get(URL, headers=headers)
-
-        if resp.status_code == 200:
-            soup = BeautifulSoup(resp.content, "html.parser")
-            results = []
-            for g in soup.find_all('div', class_='r'):
-                anchors = g.find_all('a')
-                if anchors:
-                    link = anchors[0]['href']
-                    title = g.find('h3').text
-                    item = {
-                        "link": link
-                    }
-                    results.append(item)
-            print(*results, sep = '\n')
-            print(Fore.GREEN + '[+] Total vulnerable websites found : %s ' % str( len( results ) ) ) 
-            print(" ")            
-            print(Style.RESET_ALL)
-            print(Fore.RED + "[+] !!! WARNING !!!\n")
-            print(Fore.RED + "[+] Always check and analyse your victim before attacking it.\n")
-            print(Fore.RED + "[+] Never attack a website you don't have permission for.\n")
-            print(Style.RESET_ALL)
-            time.sleep(2.0)
-
-            print(Fore.GREEN + "[+] LOADING OTHER WEBSITES...\n")
-            print(Style.RESET_ALL)
-            time.sleep(3.0)
+    	for line in fin:
     
+        	query = line
+        	query = query.replace(' ', '+')
+        	URL = f"https://google.com/search?q={query}"
+
+        	headers = {"user-agent": USER_AGENT}
+        	resp = requests.get(URL, headers=headers)
+
+        	if resp.status_code == 200:
+            		soup = BeautifulSoup(resp.content, "html.parser")
+            		results = []
+            		for g in soup.find_all('div', class_='r'):
+                		anchors = g.find_all('a')
+                		if anchors:
+                    			link = anchors[0]['href']
+                    			title = g.find('h3').text
+                    			item = {
+                        			"link": link
+                    			}
+                    			results.append(item)
+            		print(*results, sep = '\n')
+            		print(Fore.GREEN + '[+] Total vulnerable websites found : %s ' % str( len( results ) ) ) 
+            		print(" ")            
+            		print(Style.RESET_ALL)
+            		print(Fore.RED + "[+] !!! WARNING !!!\n")
+            		print(Fore.RED + "[+] Never attack a website you don't have permission for.\n")
+            		print(Style.RESET_ALL)
+            		time.sleep(2.0)
+
+            		print(Fore.GREEN + "[+] LOADING OTHER WEBSITES...\n")
+            		print(Style.RESET_ALL)
+            		time.sleep(3.0)
+    sql()
+   
 elif b == '2':
-    print(" ")
 
-    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    def xss():
+    	print(" ")
 
-    MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+    	USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
-    fin = open('xdo.txt')
+    	MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 
-    for line in fin:
-    
-        query = line
-        query = query.replace(' ', '+')
-        URL = f"https://google.com/search?q={query}"
+    	fin = open('xdo.txt')
 
-        headers = {"user-agent": USER_AGENT}
-        resp = requests.get(URL, headers=headers)
+    	for line in fin:
 
-        if resp.status_code == 200:
-            soup = BeautifulSoup(resp.content, "html.parser")
-            results = []
-            for g in soup.find_all('div', class_='r'):
-                anchors = g.find_all('a')
-                if anchors:
-                    link = anchors[0]['href']
-                    title = g.find('h3').text
-                    item = {
-                        "link": link
-                    }
-                    results.append(item)
-            print(*results, sep = '\n')
-            print(Fore.GREEN + '[+] Total vulnerable websites found : %s ' % str( len( results ) ) ) 
-            print(" ")
-            print(Style.RESET_ALL)
-            print(Fore.RED + "[+] !!! WARNING !!!\n")
-            print(Fore.RED + "[+] Always check and analyse your victim before attacking it.\n")
-            print(Fore.RED + "[+] Never attack a website you don't have permission for.\n")
-            print(Style.RESET_ALL)
-            time.sleep(2.0)
+        	query = line
+        	query = query.replace(' ', '+')
+        	URL = f"https://google.com/search?q={query}"
 
-            print(Fore.GREEN + "[+] LOADING OTHER WEBSITES...\n")
-            print(Style.RESET_ALL)
-            time.sleep(3.0)
+        	headers = {"user-agent": USER_AGENT}
+        	resp = requests.get(URL, headers=headers)
+
+        	if resp.status_code == 200:
+            		soup = BeautifulSoup(resp.content, "html.parser")
+            		results = []
+            		for g in soup.find_all('div', class_='r'):
+                		anchors = g.find_all('a')
+                		if anchors:
+                    			link = anchors[0]['href']
+                    			title = g.find('h3').text
+                    			item = {
+                        		"link": link
+                    			}
+                    			results.append(item)
+            		print(*results, sep = '\n')
+            		print(Fore.GREEN + '[+] Total vulnerable websites found : %s ' % str( len( results ) ) ) 
+            		print(" ")
+            		print(Style.RESET_ALL)
+            		print(Fore.RED + "[+] !!! WARNING !!!\n")
+            		print(Fore.RED + "[+] Never attack a website you don't have permission for.\n")
+            		print(Style.RESET_ALL)
+            		time.sleep(2.0)
+
+            		print(Fore.GREEN + "[+] LOADING OTHER WEBSITES...\n")
+            		print(Style.RESET_ALL)
+            		time.sleep(3.0)
+    xss()
     
 else:
     print(" ")
